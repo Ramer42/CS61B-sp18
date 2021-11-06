5a9c5cf660c226e8657483ecb15907be6ca1d909
Ramer42 <1797662488@qq.com>
2021-11-06 18:36:06 +0800

    test lab13

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -17,80 +17,19 @@ public class RadixSort {
      *
      * @return String[] the sorted array
      */
-    public static String[] sort(String[] asciis) {
-        int maxLen = 0;
-        for (String s : asciis) {
-            if (s.length() > maxLen) {
-                maxLen = s.length();
-            }
-        }
-
-        String[] res = Arrays.copyOf(asciis, asciis.length);
-        for (int i = maxLen - 1; i >= 0; i--) {
-            sortHelperLSD(res, i);
-        }
-        return res;
-    }
-
-    /**
-     * LSD helper method that performs a destructive counting sort the array of
-     * Strings based off characters at a specific index.
-     * @param asciis Input array of Strings
-     * @param index The position to sort the Strings on.
-     */
-    private static void sortHelperLSD(String[] asciis, int index) {
-        // Optional LSD helper method for required LSD radix sort
-        int R = 256;
-        int[] counts = new int[R + 1];
-        for (String item : asciis) {
-            int c = charAtOrMinChar(index, item);
-            counts[c]++;
-        }
-
-        int[] starts = new int[R + 1];
-        int pos = 0;
-        for (int i = 0; i < R + 1; i++) {
-            starts[i] = pos;
-            pos += counts[i];
-        }
-
-        String[] sorted = new String[asciis.length];
-        for (int i = 0; i < asciis.length; i++) {
-            String item = asciis[i];
-            int c = charAtOrMinChar(index, item);
-            int place = starts[c];
-            sorted[place] = item;
-            starts[c]++;
-        }
-
-        for (int i = 0; i < asciis.length; i++) {
-            asciis[i] = sorted[i];
-        }
-    }
-
-    private static int charAtOrMinChar(int index, String item) {
-        if (index < item.length() && index >= 0) {
-            return item.charAt(index) + 1;
-        } else {
-            return 0;
-        }
-    }
-//    private static int maxLength = Integer.MIN_VALUE;
 //    public static String[] sort(String[] asciis) {
-//        // TODO: Implement LSD Sort
-//        // make a copy of the original asciis to keep non-destructive
-//        String[] arrCopy = new String[asciis.length];
-//        System.arraycopy(asciis, 0, arrCopy, 0, asciis.length);
-//
-//        // find the max length of all strings in the asciis
-//        for (String ascii : asciis) {
-//            maxLength = Math.max(ascii.length(), maxLength);
+//        int maxLen = 0;
+//        for (String s : asciis) {
+//            if (s.length() > maxLen) {
+//                maxLen = s.length();
+//            }
 //        }
 //
-//        for (int d = 0; d < maxLength; d++) {
-//            sortHelperLSD(arrCopy, maxLength - d - 1);
+//        String[] res = Arrays.copyOf(asciis, asciis.length);
+//        for (int i = maxLen - 1; i >= 0; i--) {
+//            sortHelperLSD(res, i);
 //        }
-//        return arrCopy;
+//        return res;
 //    }
 //
 //    /**
@@ -101,53 +40,114 @@ public class RadixSort {
 //     */
 //    private static void sortHelperLSD(String[] asciis, int index) {
 //        // Optional LSD helper method for required LSD radix sort
-//        // make a new asciis in which pad every shorter strings with '_'
-//        String[] asciisPadded = new String[asciis.length];
-//        for (int i = 0; i < asciis.length; i += 1) {
-//            if (asciis[i].length() < maxLength) {
-//                asciisPadded[i] = pad(asciis[i], maxLength - asciis[i].length());
-//            } else {
-//                asciisPadded[i] = asciis[i];
-//            }
+//        int R = 256;
+//        int[] counts = new int[R + 1];
+//        for (String item : asciis) {
+//            int c = charAtOrMinChar(index, item);
+//            counts[c]++;
 //        }
 //
-//        // gather all the counts for each value
-//        int[] counts = new int[257];
-//        for (String i : asciisPadded) {
-//            counts[i.charAt(index)]++;
-//        }
-//
-//        // calculate start position
-//        int[] starts = new int[257];
+//        int[] starts = new int[R + 1];
 //        int pos = 0;
-//        for (int i = 0; i < starts.length; i += 1) {
+//        for (int i = 0; i < R + 1; i++) {
 //            starts[i] = pos;
 //            pos += counts[i];
 //        }
 //
 //        String[] sorted = new String[asciis.length];
-//        for (int i = 0; i < asciis.length; i += 1) {
-//            String item = asciisPadded[i];
-//            int place = starts[item.charAt(index)];
-//            sorted[place] = asciis[i];  // remember to use the original asciis without '_'
-//            starts[item.charAt(index)] += 1;
+//        for (int i = 0; i < asciis.length; i++) {
+//            String item = asciis[i];
+//            int c = charAtOrMinChar(index, item);
+//            int place = starts[c];
+//            sorted[place] = item;
+//            starts[c]++;
 //        }
-//        System.arraycopy(sorted, 0, asciis, 0, sorted.length);
+//
+//        for (int i = 0; i < asciis.length; i++) {
+//            asciis[i] = sorted[i];
+//        }
 //    }
 //
-//    /**
-//     * pad the string s with '_' at the back end,
-//     * for example pad("2", 2) will return "2__"
-//     * @param s the string to be padded
-//     * @param numberOfPad the number of '_'
-//     * @return padded string
-//     */
-//    private static String pad(String s, int numberOfPad) {
-//        for (int i = 0; i < numberOfPad; i++) {
-//            s += '_';
+//    private static int charAtOrMinChar(int index, String item) {
+//        if (index < item.length() && index >= 0) {
+//            return item.charAt(index) + 1;
+//        } else {
+//            return 0;
 //        }
-//        return s;
 //    }
+    private static int maxLength = Integer.MIN_VALUE;
+    public static String[] sort(String[] asciis) {
+        // TODO: Implement LSD Sort
+        // make a copy of the original asciis to keep non-destructive
+        String[] arrCopy = new String[asciis.length];
+        System.arraycopy(asciis, 0, arrCopy, 0, asciis.length);
+
+        // find the max length of all strings in the asciis
+        for (String ascii : asciis) {
+            maxLength = Math.max(ascii.length(), maxLength);
+        }
+
+        for (int d = 0; d < maxLength; d++) {
+            sortHelperLSD(arrCopy, maxLength - d - 1);
+        }
+        return arrCopy;
+    }
+
+    /**
+     * LSD helper method that performs a destructive counting sort the array of
+     * Strings based off characters at a specific index.
+     * @param asciis Input array of Strings
+     * @param index The position to sort the Strings on.
+     */
+    private static void sortHelperLSD(String[] asciis, int index) {
+        // Optional LSD helper method for required LSD radix sort
+        // make a new asciis in which pad every shorter strings with '_'
+        String[] asciisPadded = new String[asciis.length];
+        for (int i = 0; i < asciis.length; i += 1) {
+            if (asciis[i].length() < maxLength) {
+                asciisPadded[i] = pad(asciis[i], maxLength - asciis[i].length());
+            } else {
+                asciisPadded[i] = asciis[i];
+            }
+        }
+
+        // gather all the counts for each value
+        int[] counts = new int[257];
+        for (String i : asciisPadded) {
+            counts[i.charAt(index)]++;
+        }
+
+        // calculate start position
+        int[] starts = new int[257];
+        int pos = 0;
+        for (int i = 0; i < starts.length; i += 1) {
+            starts[i] = pos;
+            pos += counts[i];
+        }
+
+        String[] sorted = new String[asciis.length];
+        for (int i = 0; i < asciis.length; i += 1) {
+            String item = asciisPadded[i];
+            int place = starts[item.charAt(index)];
+            sorted[place] = asciis[i];  // remember to use the original asciis without '_'
+            starts[item.charAt(index)] += 1;
+        }
+        System.arraycopy(sorted, 0, asciis, 0, sorted.length);
+    }
+
+    /**
+     * pad the string s with '_' at the back end,
+     * for example pad("2", 2) will return "2__"
+     * @param s the string to be padded
+     * @param numberOfPad the number of '_'
+     * @return padded string
+     */
+    private static String pad(String s, int numberOfPad) {
+        for (int i = 0; i < numberOfPad; i++) {
+            s += '_';
+        }
+        return s;
+    }
 
 //    /**
 //     * MSD radix sort helper function that recursively calls itself to achieve the sorted array.
